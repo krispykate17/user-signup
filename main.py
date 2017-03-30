84d46f0fac0607c7d506ca6c58bdcdf55fb45653
Katie Juhala <kajuhala@gmail.com>
2017-03-30 17:29:48 -0500

    When user types in username, they are brought to welcome page

--- main.py
+++ main.py
@@ -16,10 +16,53 @@
 #
 import webapp2
 
+def valid_username(username):
+    return username
+
+def valid_password(password):
+    return password
+
+def valid_email(email):
+    return email
+
+def build_page(username, email):
+    username_label ="<label>Username</label>"
+    username = "<input type='text' name='username'/>"
+
+    password_label ="<label>Password</label>"
+    password = "<input type='text' name='password'>"
+
+    verify_label ="<label>Verify Password</label>"
+    verify = "<input type='text' name='verify'>"
+
+    email_label ="<label>Email (optional)</label>"
+    email = "<input type='text' name='email'>"
+
+    submit = "<input type='submit'/>"
+
+    form = ("<form action='/welcome' method='post'>" + username_label + username + "<br>" +
+                password_label + password + "<br>" + verify_label +
+                verify + "<br>" + email_label + email + "<br>" +
+                submit + "</form>")
+
+    header = "<h1>Signup</h1>"
+    return header + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page("", "")
+        self.response.write(content)
+
+class Welcome(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get("username")
+        #rotation = int(self.request.get("rotation"))
+        #encrypted_message = caesar.encrypt(message, rotation)
+        #escaped_msg = cgi.escape(encrypted_message)
+        #content = build_page() <---(Maybe keep for if user inputs something stupid?
+        content = "<h1>Welcome, " + username + "!</h1>"
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/welcome', Welcome)
 ], debug=True)
